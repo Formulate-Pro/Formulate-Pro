7f3b5900b38dbe1137881cb3efd5363d84487ba9
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2020-05-24 20:08:19 -0700

    feat (NuGet) install Formulate.Binaries NuGet package in the Formulate.Pro project
    Also, add Formulate.Pro.Meta project to manage the version number.

---
+++ src/Formulate.Pro.Meta/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace Formulate.Pro.Meta
+{
+
+    /// <summary>
+    /// Constants relating for Formulate Pro itself (i.e., does not include constants used by
+    /// Formulate Pro).
+    /// </summary>
+    public class Constants
+    {
+
+        /// <summary>
+        /// This is the version of Formulate Pro. It is used on assemblies, and during the
+        /// creation of the installer package.
+        /// </summary>
+        public const string Version = "1.0.0";
+
+    }
+}
--- src/Formulate.Pro.Meta/Properties/AssemblyInfo.cs
+++ src/Formulate.Pro.Meta/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Formulate.Pro")]
+[assembly: AssemblyTitle("Formulate.Pro.Meta")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Formulate.Pro")]
+[assembly: AssemblyProduct("Formulate.Pro.Meta")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("661c9772-a9a1-4843-ba1e-5c8c459df664")]
+[assembly: Guid("29b21135-b06d-4ec5-a6ff-dcc23eebee03")]
 
 // Version information for an assembly consists of the following four values:
 //
